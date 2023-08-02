20aa64d79f9b87d4d0ad2d5db802b0f25c331164
Leroy G <liguan698@gmail.com>
2023-08-02 15:27:52 -0700

    complete lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode="w") as new_file:
+        new_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode="a") as new_file:
+        new_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt') as new_file:
+        return new_file.read()
+
